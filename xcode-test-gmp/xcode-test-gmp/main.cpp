ccad031c2e96ddb2e6ed363db870c5ebdad44b3a
Steve Tarzia <steve@stevetarzia.com>
2017-03-30 16:32:32 -0500

    added GMP code to Xcode project

--- xcode-test-gmp/xcode-test-gmp/main.cpp
+++ xcode-test-gmp/xcode-test-gmp/main.cpp
@@ -6,10 +6,23 @@
 //  Copyright © 2017 Kellogg | Northwestern. All rights reserved.
 //
 
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
+#include <gmp.h>
+#include <gmpxx.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main () {
+    mpz_class a, b, c;
+    
+    a = 1234;
+    b = "-5678";
+    c = a+b;
+    
+    cout << "sum of " << a << " and " << b << " is " << c << "\n";
+    cout << "absolute value is " << abs(c) << "\n";
+    return 0; 
 }
