c64c59f87c2c761dc975e18ff7c3405600446a6f
Steve Tarzia <steve@stevetarzia.com>
2017-03-30 15:21:25 -0500

    made example actually print something

--- test.cpp
+++ test.cpp
@@ -7,10 +7,14 @@
 
 using namespace std;
 
-int main ()
-{
-    mpz_t p;
-    mpz_init_set_ui (p,3);
-
-    return 0;
+int main () {
+  mpz_class a, b, c;
+  
+  a = 1234;
+  b = "-5678";
+  c = a+b;
+  
+  cout << "sum of " << a << " and " << b << " is " << c << "\n";
+  cout << "absolute value is " << abs(c) << "\n";
+  return 0; 
 }
