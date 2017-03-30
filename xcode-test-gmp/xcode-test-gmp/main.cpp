f3f7f69c792ae43dac740addcbed42e9e6397839
Steve Tarzia <steve@stevetarzia.com>
2017-03-30 16:28:38 -0500

    created minimal xcode project

---
+++ xcode-test-gmp/xcode-test-gmp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xcode-test-gmp
+//
+//  Created by Stephen Tarzia on 3/30/17.
+//  Copyright © 2017 Kellogg | Northwestern. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
